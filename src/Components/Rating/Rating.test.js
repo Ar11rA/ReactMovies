a8e7656b1da1685bc395d6a8cc81bf30ac94a09f
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-03-02 12:27:25 +0530

    Tested the ratings file

--- src/Components/Rating/Rating.test.js
+++ src/Components/Rating/Rating.test.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom'
 import Rating from './Rating'
 import { shallow } from 'enzyme'
 it('has a span', () => {
-  const app = shallow(<Rating stars={5}/>)
+  const app = shallow(<Rating stars={5} />)
   expect(app.find('span').length).toBe(1)
 })
+it('will print the received prop value and show it as a rating on 5', () => {
+  const app = shallow(<Rating stars={3} />)
+  expect(app.find('span').text()).toBe('3/5')
+})
+it('will have class red when rating is less than equal to 2', () => {
+  const app = shallow(<Rating stars={2} />)
+  expect(app.find('span').node.props.className).toBe('red')
+})
+it('will have class blue when rating is 3 or 4', () => {
+  const app = shallow(<Rating stars={3} />)
+  expect(app.find('span').node.props.className).toBe('blue')
+})
+it('will have class green when rating is equal to 5', () => {
+  const app = shallow(<Rating stars={5} />)
+  expect(app.find('span').node.props.className).toBe('green')
+})
+
